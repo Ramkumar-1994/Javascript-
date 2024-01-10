9d6a888ff567e21029fd83e12b47a4c2f9945673
Ramkumar-1994 <nramkumar1994@gmail.com>
2024-01-10 11:39:03 +0530

    javascript variables

---
+++ variables/script.js
@@ -0,0 +1,17 @@
+// Defining variables with const,let,var
+// let,const introduced in ECMA
+
+const accountId=14463
+console.log(accountId)
+let accountEmail="ram@gmail.com"
+console.log(accountEmail)
+var accountPassword="12345"
+console.log(accountPassword)
+let accountCity="Jaipur"
+console.log(accountCity)
+
+// accountId=12546 //not allowed to change variable in const
+accountEmail="rm@gmail.com"
+
+console.log(accountEmail)
+console.log(accountId,accountEmail,accountPassword,accountCity)
