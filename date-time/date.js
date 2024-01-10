1cbdce8dcbdad120c39c9edfabdad893fbb24c16
Ramkumar-1994 <nramkumar1994@gmail.com>
2024-01-10 23:44:52 +0530

    date

---
+++ date-time/date.js
@@ -0,0 +1,10 @@
+let myDate=new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDate=new Date();
+console.log(myCreatedDate.toDateString())
+console.log(myCreatedDate.toLocaleString())
+
