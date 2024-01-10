c931359359134f4e6d8c1715d408b1b1d93a6dd2
Ramkumar-1994 <nramkumar1994@gmail.com>
2024-01-10 11:56:45 +0530

    datatypes

---
+++ Datatypes/data.js
@@ -0,0 +1,15 @@
+// alert(3+3)//Dont run in node js environment ,run only in browser.
+console.log(3+3);//Number
+let lang="Javascript"
+console.log(typeof(lang))
+console.log(lang)
+
+let name="Harish"   //string
+let age=18          //Number
+let isLogged=true   //Boolean
+//undefined //value not defined
+//null=> standalone value
+//symbol=>unique
+
+//Object
+
