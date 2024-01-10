fd7dd33771dba0671ae881bfc04c77de9cf6fdbb
Ramkumar-1994 <nramkumar1994@gmail.com>
2024-01-10 16:13:36 +0530

    summary

---
+++ Datatypes/summary.js
@@ -0,0 +1,49 @@
+//Primitive
+
+//7 types:String,Number,Boolean,null,undefined,symbol,Bigint.
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+
+const anotherId=Symbol('123')
+console.log(typeof id)
+console.log(id===anotherId)
+//Reference or Non-Primitive
+
+//Array,Objects,Functions
+
+ const fruits=["Apple",'Orange','Banana']
+
+ let myObj={
+  name:"harish",
+  age:22
+ }
+
+ const myFunction=function()
+ {
+  console.log("Hello World")
+ }
+ 
+ myFunction()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
